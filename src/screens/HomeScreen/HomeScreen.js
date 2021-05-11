b75d984af06489aab2562030e1fc55c51af74f38
Omar <omaar_5@hotmail.com>
2021-05-11 16:12:30 +0200

    feat: use Redux

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,4 +1,15 @@
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { makeSelectUsers } from "./selectors";
+
+const stateSelector = createSelector(makeSelectUsers, users => ({
+    users
+}));
+
 const HomeScreen = () => {
+    const { users } = useSelector(stateSelector);
+    console.log("🚀 ~ file: HomeScreen.js ~ line 11 ~ HomeScreen ~ users", users)
+
     return (
         <div>
             HomeScreen
