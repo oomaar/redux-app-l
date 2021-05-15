24418f5229573f03f0dd959412fccd7a15370e7f
Omar <omaar_5@hotmail.com>
2021-05-16 00:31:25 +0200

    feat: homescreen reducer part-2

--- src/Redux/reducers/HomeScreenReducer.js
+++ src/Redux/reducers/HomeScreenReducer.js
@@ -1,9 +1,17 @@
+import { ActionTypes } from "../constants/HomeScreenConstants";
+
 const defaultState = {
     users: ["No User"]
 };
 
 export default function HomeScreenReducer (state = defaultState, action) {
     switch (action.type) {
+        case ActionTypes.SET_USERS:
+            return {
+                ...state,
+                users: action.payload
+            };
+
         default:
             return state;
     }
