a8c537d5640b2b00f0d1641dc081d4c2f2a1faf9
Omar <omaar_5@hotmail.com>
2021-05-15 23:07:41 +0200

    feat: basic reducer

---
+++ src/Redux/reducers/HomeScreenReducer.js
@@ -0,0 +1,10 @@
+const defaultState = {
+
+};
+
+export default function HomeScreenReducer (state = defaultState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+} ;
