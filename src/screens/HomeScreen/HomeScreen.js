4604df87ca0c8419a7a0664330fcb82b6f8c9bcc
Omar <omaar_5@hotmail.com>
2021-05-16 00:23:30 +0200

    feat: fetch the data from reqres.in api

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,17 +1,27 @@
 import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
 import { makeSelectUsers } from "../../Redux/selectors/HomeScreenSelectors";
+import Axios from "axios";
+import { useEffect } from "react";
 
-const stateSelector = createSelector(makeSelectUsers, users => (
-    {
+const stateSelector = createSelector(makeSelectUsers, users => ({
         // users: users,
         users
-    }
-));
+    }));
 
 const HomeScreen = () => {
     const { users } = useSelector(stateSelector);
-    console.log("🚀 ~ file: HomeScreen.js ~ line 14 ~ HomeScreen ~ users", users)
+
+    const fetchUsers = async () => {
+        const response = await Axios.get("https://reqres.in/api/users")
+        .catch(error => console.error("error: ", error));
+
+        console.log(response.data.data)
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
 
     return (
         <div>
