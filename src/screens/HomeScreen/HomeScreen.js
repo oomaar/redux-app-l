402e9c9660b8f05ed96db60f6d0e15a357f9247e
Omar <omaar_5@hotmail.com>
2021-05-16 15:40:35 +0200

    fix: es-lint issue

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useDispatch, useSelector } from "react-redux";
 import { createSelector } from "reselect";
 import { makeSelectStatus, makeSelectUsers } from "../../Redux/selectors/HomeScreenSelectors";
