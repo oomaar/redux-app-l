4d386a55451707cbf943f79fd729f9e56efe2d54
Omar <omaar_5@hotmail.com>
2021-05-16 15:38:13 +0200

    feat: true/false state constatnt selector

--- src/Redux/selectors/HomeScreenSelectors.js
+++ src/Redux/selectors/HomeScreenSelectors.js
@@ -3,3 +3,5 @@ import { createSelector } from "reselect";
 const homeScreenState = (state) => state.HomeScreenReducer;
 
 export const makeSelectUsers = createSelector( homeScreenState, homeScreen => homeScreen.users );
+
+export const makeSelectStatus = createSelector( homeScreenState, homeScreen => homeScreen.status );
