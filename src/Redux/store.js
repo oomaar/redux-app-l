ebb9d3cdd96f13420cc366dc9bdf47f021f766d6
Omar <omaar_5@hotmail.com>
2021-05-16 16:29:09 +0200

    feat: Include Math redux state in Store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,10 @@
 import { createStore, combineReducers } from "redux";
 import HomeScreenReducer from "./reducers/HomeScreenReducer";
+import MathReducers from "./reducers/MathReducers";
 
-const reducers = combineReducers({ HomeScreenReducer });
+const reducers = combineReducers({
+    HomeScreenReducer,
+    MathReducers
+});
 
 export default createStore(reducers);
