6b298d463d0dafda61a02101abcb32533c3706ac
Omar <omaar_5@hotmail.com>
2021-05-16 18:07:38 +0200

    feat: display data on screen

--- src/Components/UsersList/UsersList.js
+++ src/Components/UsersList/UsersList.js
@@ -1,13 +1,38 @@
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { makeSelectUsers } from "../../Redux/selectors/HomeScreenSelectors";
 import {
     UsersListSection,
     Container,
+    UserWrapper,
+    UserImage,
+    UserName,
 } from "./styledUsersList";
 
+const stateSelector = createSelector(makeSelectUsers, users => ({ users }));
+
 const UsersList = () => {
+    const { users } = useSelector(stateSelector);
+
+    const isEmptyUsers = !users || (users && users.length === 0);
+
+    if (isEmptyUsers) {
+        return null;
+    };
+
     return (
         <UsersListSection>
             <Container>
-                UsersList
+                {users.map((user, idx) => (
+                    <UserWrapper key={idx}>
+                        <UserImage>
+                            <img src={user.avatar} alt={user.first_name} />
+                        </UserImage>
+                        <UserName>
+                            {user.first_name} {user.last_name}
+                        </UserName>
+                    </UserWrapper>
+                ))}
             </Container>
         </UsersListSection>
     );
--- src/Components/UsersList/styledUsersList.js
+++ src/Components/UsersList/styledUsersList.js
@@ -2,4 +2,32 @@ import styled from "styled-components/macro";
 
 export const UsersListSection = styled.section``;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    padding: 20px 0;
+`;
+
+export const UserWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const UserImage = styled.div`
+    width: 7rem;
+    height: 7rem;
+    margin: 0 20px;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+export const UserName = styled.h3`
+    font-size: 1.25rem;
+    color: #000;
+    margin: 0;
+`;
