52ac95c2236687dfcca66e1f796986ec43ab253a
Omar <omaar_5@hotmail.com>
2021-05-16 16:28:13 +0200

    feat: Math Constatns

---
+++ src/Redux/constants/MathConstants.js
@@ -0,0 +1,5 @@
+export const MathActionTypes = {
+    INCREMENT: "app/components/Math/Math/INCREMENT",
+    DECREMENT: "app/components/Math/Math/DECREMENT",
+    INC_BY_TEN: "app/components/Math/Math/INC_BY_TEN"
+}
